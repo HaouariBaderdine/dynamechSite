33d0efd34e07b2cc997341875ec94a66e9ba5e0a
HaouariBaderdine <mimi.bader4@gmail.com>
2020-11-09 22:18:45 +0100

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,22 @@
+
+jQuery(document).ready(function() {
+	
+	/*
+	    Wow
+	*/
+	new WOW().init();
+	
+	/*
+	    Slider
+	*/
+	$('.flexslider').flexslider({
+        animation: "slide",
+        controlNav: "thumbnails",
+        prevText: "",
+        nextText: ""
+    });
+    
+	
+});
+
+
